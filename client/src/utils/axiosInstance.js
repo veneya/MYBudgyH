366186c8c61b58c86dec91ce31af1688edfd3414
veneya <veneya2005@gmail.com>
2026-03-20 00:27:55 +0530

    add render backend url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://mybudgyh.onrender.com",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",

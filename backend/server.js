1cbefeb7c7c746a46d2ffdd380aba8c82fcffed2
veneya <veneya2005@gmail.com>
2026-07-20 03:02:33 +0530

    Rate liniter added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require('express');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
+
 const app = require('./app');
 const connectDB = require('./db');
 
@@ -10,12 +12,22 @@ const incomeRoutes = require('./routes/income.routes');
 const expensesRoutes = require('./routes/expenses.routes');
 const budgetRoutes = require('./routes/budget.routes');
 const transactionRoutes = require('./routes/transaction.routes');
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: {
+        error: 'Too many requests from this IP, please try again later.'
+    },
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // Import middlewares
 const noCache = require('./middleware/noCache.middleware');
 
 const PORT = process.env.PORT || 8000;
 
+app.use('/api', limiter);
 app.use('/api', noCache);
 
 app.use('/api/auth', authRoutes);

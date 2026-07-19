633bd48f06ebd967983ddf2d82c9f073a589d621
Veneya <150456466+veneya@users.noreply.github.com>
2026-07-20 01:31:17 +0530

    backend deployed link updated

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",  // for local development
+    baseURL: "https://mybudgyh.onrender.com",  // for local development
     timeout: 10000,
     headers: { "Content-Type": "application/json" },
 });
